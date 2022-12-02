57983ff0f4c4327454200f3158c0f135af478054
Fadhli Azhari <m.fadhliazhari@gmail.com>
2022-12-02 17:43:47 +0800

    Added timer interrupt handler

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,8 @@ pub mod vga_buffer;
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
