29bef153605c51be81094a9110c3f1446b5f96c6
Fadhli Azhari <m.fadhliazhari@gmail.com>
2022-12-02 18:31:45 +0800

    Add memory paging helper function

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,31 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     rust_os_journey::init();
 
+    use rust_os_journey::memory;
+    use x86_64::{structures::paging::Translate, VirtAddr};
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // new: initialize a mapper
+    let mapper = unsafe { memory::init(phys_mem_offset) };
+
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        // new: use the `mapper.translate_addr` method
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
+    }
+
     #[cfg(test)]
     test_main();
 
